30581dc3d9e2efa228cfb7dc78b77b19b31a1316
Marco Pinto <marco.pinto@iakta.it>
2025-01-27 02:43:07 +0100

    final back office

--- build.sh
+++ build.sh
@@ -17,7 +17,7 @@ import os;
 from django.contrib.auth.models import User;
 username = os.getenv('SUPERUSER_USERNAME', 'admin');
 email = os.getenv('SUPERUSER_EMAIL', 'admin@example.com');
-password = os.getenv('SUPERUSER_PASSWORD', 'adminpassword');
+password = os.getenv('SUPERUSER_PASSWORD', 'admin');
 if not User.objects.filter(username=username).exists():
     User.objects.create_superuser(username=username, email=email, password=password)
     print(f'Superuser {username} created.')
