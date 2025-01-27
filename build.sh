c323342ab2fb26148acc29f3444d591f4ae24426
Marco Pinto <marco.pinto@iakta.it>
2025-01-27 02:39:38 +0100

    final back office

--- build.sh
+++ build.sh
@@ -11,4 +11,16 @@ python manage.py collectstatic --no-input
 # Apply any outstanding database migrations
 python manage.py migrate
 
-python manage.py runscript create_superuser
+# python manage.py runscript create_superuser
+python manage.py shell -c "
+import os;
+from django.contrib.auth.models import User;
+username = os.getenv('SUPERUSER_USERNAME', 'admin');
+email = os.getenv('SUPERUSER_EMAIL', 'admin@example.com');
+password = os.getenv('SUPERUSER_PASSWORD', 'adminpassword');
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username=username, email=email, password=password)
+    print(f'Superuser {username} created.')
+else:
+    print(f'Superuser {username} already exists.')
+"
