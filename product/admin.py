1618b28d25913ae4ac180495d8e6ef702ae27269
Marco Pinto <marco.pinto@iakta.it>
2025-09-19 13:17:26 +0200

    work in progress and tags3

--- product/admin.py
+++ product/admin.py
@@ -21,6 +21,7 @@ class ProductAdmin(admin.ModelAdmin):
         return "No barcode"
 
     barcode_image.short_description = 'Barcode'
+    # prova
 
 admin.site.register(product_models.Product, ProductAdmin)
 admin.site.register(product_models.ProductHistory)
